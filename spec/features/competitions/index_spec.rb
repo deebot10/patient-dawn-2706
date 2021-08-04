a8daf90b0ca5620cdae97a320e386ed682bc432b
deebot10 <dee1jhill@gmail.com>
2021-08-04 09:27:04 -0600

    Create CompetitionsController

---
+++ spec/features/competitions/index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Competition Index' do
+  before(:each) do
+    @competition_1 = Competition.create!(name: 'Mens Regional', location: 'Denver', sport: 'Basketball')  
+    @competition_2 = Competition.create!(name: 'Womans Regional', location: 'Houston', sport: 'Baseball')  
+    @competition_3 = Competition.create!(name: 'Nationals', location: 'Chicago', sport: 'Water Polo')  
+
+    visit '/competitions'
+  end
+
+  describe 'Story1' do
+    it 'displays the names of all competitions' do
+
+      expect(page).to have_content(@competition_1.name)
+      expect(page).to have_content(@competition_2.name)
+      expect(page).to have_content(@competition_3.name)
+    end
+  end
+end
