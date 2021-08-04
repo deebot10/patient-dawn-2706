126e164590e35df3415d521b9742a0233e56d919
deebot10 <dee1jhill@gmail.com>
2021-08-04 09:46:58 -0600

    Pass part of story2

--- spec/features/competitions/show_spec.rb
+++ spec/features/competitions/show_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe 'Competition Show Page' do
   end
 
   describe 'Story2' do
+    #     User Story 2 - Competition Show Page
+
+    # As a user
+    # When I visit a competition's show page
+    # Then I see the competition's name, location, and sport
+    # And I see the nickname and hometown of all teams in this competition
+    # And I see the average age of all players in the competition
     it 'displays a competition and attributes' do
       expect(page).to have_content(@competition.name)
       expect(page).to have_content(@competition.location)
