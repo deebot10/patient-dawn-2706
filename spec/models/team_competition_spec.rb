6c60040483fd63c8e48a6d6735ce014d79b5c4b9
deebot10 <dee1jhill@gmail.com>
2021-08-04 09:18:59 -0600

    Pass model relationship test

--- spec/models/team_competition_spec.rb
+++ spec/models/team_competition_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe TeamCompetitions do
+RSpec.describe TeamCompetition do
   it { should belong_to :team}
-  it { should belong_to :team_competition}
+  it { should belong_to :competition}
 end
